8bcc01bfbf350abcde4d8c24b96df351bee0dbdf
Jack Wagner <jacksonjw519@gmail.com>
2022-11-30 08:47:12 -0500

    AWS hue_flask updated

--- create_color_rgb_db.py
+++ create_color_rgb_db.py
@@ -1,5 +1,12 @@
-import boto3
+"""
+This program creates our ColorsRGB DynamoDB Table in AWS.
+Uses our colors.csv file locally to create a cloud Database used for looking up color RGB values.
+Entry Example:
+    - {"Color": "Blue", "R":46, "B":230, "G":180}
+Program handles the creation and loading of the table.
 
+"""
+import boto3
 
 def set_up():
     return boto3.resource("dynamodb"), boto3.client("dynamodb")
@@ -8,7 +15,7 @@ def set_up():
 def create_table(resource, tbl_list, tbl_name):
     if tbl_name not in tbl_list:
         # ALL ATTRIBUTES WILL BE SAVED WITH UPPER CASE LETTERS
-        partition_key= "PHONE-NUMBER"
+        partition_key = "Color"
         table = resource.create_table(
             TableName=tbl_name,
             KeySchema=[
@@ -41,13 +48,26 @@ def delete_table(client, tbl_name):
     except (Exception):
         print("Error deleting table, resource not found")
 
+
+def load_data(resource):
+    table = resource.Table("ColorsRGB")
+    colors_file = open("./hue_sms/src/colors.csv")
+    for line in colors_file.readlines():
+        line = line.strip()
+        color, R,G,B = line.split(',')
+        item_json = {"Color":color, "R":int(R), "G":int(G), "B":int(B)}
+        table.put_item(Item=item_json)
+
+
 def get_table_list(client):
     return list(client.list_tables()["TableNames"])
 
+
 def main():
     resource, client = set_up()
-    # TABLE NAMES WILL BE SAVED WITH TITLE LETTER CASE
-    create_table(resource, get_table_list(client), "HueLightUserData")
+    create_table(resource, get_table_list(client), "ColorsRGB")
+    load_data(resource)
+
 
 if __name__ == '__main__':
     main()
