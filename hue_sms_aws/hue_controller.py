62ca2f63e27219f91f2928ac24f69f6375ed8c67
Jack Wagner <jacksonjw519@gmail.com>
2022-11-30 09:03:14 -0500

    New directory for hue_sms_aws

---
+++ hue_sms_aws/hue_controller.py
@@ -0,0 +1,32 @@
+from phue import Bridge, PhueException
+import name_converter
+from dynaconf import Dynaconf
+import logging
+
+logging.basicConfig(level=logging.INFO,filename="hue_log.log",
+                    format="%(asctime)s:%(levelname)s:%(message)s"	)
+
+settings = Dynaconf(settings_files=['settings.toml'])
+
+saturation_val = 0
+branch_value = 0
+IP_address = settings.light_ip
+light_number = settings.light_number
+
+
+class HueController:
+
+    def __init__(self):
+        self.bridge = None
+        self.light = None
+        self.name_to_color = name_converter.NameConverter()
+
+    def connect(self):
+        if self.light is not None:
+            return
+
+        self.bridge = Bridge(IP_address)
+        self.bridge.connect()
+        logging.info("Server was successfully able to connect to the bridge")
+        self.light = self.bridge.lights[light_number]
+
---
+++ hue_sms_aws/hue_flask_aws.py
@@ -0,0 +1,42 @@
+from phue import PhueException
+from twilio.twiml.messaging_response import MessagingResponse
+from flask import Flask, request
+from hue_controller import HueController
+import logging
+
+logging.basicConfig(level=logging.INFO, filename="hue_log.log",
+                    format="%(asctime)s:%(levelname)s:%(message)s")
+
+app = Flask(__name__)
+controller = HueController()
+
+
+@app.route('/', methods=['POST', 'GET'])
+def set_color():
+    print(request.json["Body"])
+    x, y, saturation_val = request.json["Body"].split(" ")
+    x, y, saturation_val = float(x), float(y), int(saturation_val)
+    try:
+        controller.connect()
+    except PhueException:
+        logging.info("Server unable to connect to the Hue Light")
+        response = MessagingResponse()
+        response.message("Server unable to connect to the Hue Light")
+        return str(response)
+
+    try:
+        controller.light.xy = (x, y)
+        controller.light.saturation = saturation_val
+        logging.info("The light was changed")
+
+    except PhueException:
+        logging.info("Server unable to connect to the Hue Light")
+        response = MessagingResponse()
+        response.message("I'm sorry, but I cannot connect to the Hue Light. Please try again later.")
+        return str(response)
+    return "success"
+
+
+if __name__ == '__main__':
+    app.run()
+    logging.info("Server has been stopped")
