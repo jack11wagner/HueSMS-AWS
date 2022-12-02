a73c74e62a168a82dbb5a01f86d7c937d06810b2
Jack Wagner <jacksonjw519@gmail.com>
2022-12-01 23:35:34 -0500

    changes to infrastructure bash script

--- hue_sms_dynamo_infrastructure.sh
+++ hue_sms_dynamo_infrastructure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ./get_data_from_pi.sh
-python3 create_db.py
+python3 create_user_data_db.py
+python3 create_color_rgb_db
 echo "DynamoDB Table Initializing..."
 sleep 10
-python3 upload_data.py
